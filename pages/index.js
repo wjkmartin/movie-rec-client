cbeccc3dfbd1281d2a7c1d40e98499f6605b2047
William Martin <wjkmartin@gmail.com>
2022-01-24 20:19:02 -0800

    feat: add login dialogue

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,11 @@ import SignInScreen from '../components/Auth/firestoreHelpers';
 import commonStyles from '../styles/common.module.css';
 
 import Container from '@mui/material/Container';
-import LoginBlock from '../components/Auth/loginBlock';
+
 
 export default function Home() {
   return (
     <Container commonStyles={commonStyles.ContainerLoading}> 
-      <LoginBlock />
     </Container>
   );
 }
