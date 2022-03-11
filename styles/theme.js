84f40dd5cee1518d9f0b1f38caf3da7de9c86baf
William Martin <wjkmartin@gmail.com>
2022-03-11 10:08:02 -0800

    feat: extract theme from _app

---
+++ styles/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ff9800',
+      },
+      secondary: {
+        main: '#ff9800',
+      },
+      success: {
+        main: '#009688',
+      },
+    },
+    typography: {
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+        h1: {
+            fontSize: '5.5rem',
+            fontWeight: '300',
+            lineHeight: '1.2',
+            '@media (max-width:800px)': {
+                fontSize: '3.5rem',
+              },
+        },
+    }
+  });
+
+export default theme;
